ae0d683492d2cc4f25e2b40836f397c8440887f4
Aykut Türkyılmaz <120272313+Aykuttylmz@users.noreply.github.com>
2024-12-18 20:35:03 +0300

    initial commit

---
+++ GHProject/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GHProject
+//
+//  Created by Aykut Türkyılmaz on 17.12.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+        
+    }
+
+}
